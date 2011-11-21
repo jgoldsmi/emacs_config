d6ba7bf69ea4ec939a5082fc5581fe371d31aef0
Jonathon Goldsmith <jgoldsmith@wgen.net>
2011-11-21 13:22:08 -0500

    Add magit as a submodule

--- init.el
+++ init.el
@@ -1,6 +1,11 @@
 ;; init.el
 (menu-bar-mode -1)
 (setq indent-tabs-mode nil)
+(setq inhibit-splash-screen t)
+
+;; Magit
+(add-to-list 'load-path "~/.emacs.d/magit")
+(require 'magit)
 
 ;; undo-tree
 (add-to-list 'load-path "~/.emacs.d/undo-tree")
