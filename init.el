6b87d5041f919b9db6b0dfa2ad8d753a92cd1ef7
Jonathon Goldsmith <jgoldsmith@wgen.net>
2011-11-21 14:57:14 -0500

    remove tabs

--- init.el
+++ init.el
@@ -14,13 +14,13 @@
 (when (load "flymake" t) 
   (defun flymake-pyflakes-init () 
     (let* ((temp-file (flymake-init-create-temp-buffer-copy 
-		       'flymake-create-temp-inplace)) 
-	   (local-file (file-relative-name 
-			temp-file 
-			(file-name-directory buffer-file-name)))) 
+                       'flymake-create-temp-inplace)) 
+           (local-file (file-relative-name 
+                        temp-file 
+                        (file-name-directory buffer-file-name)))) 
       (list "pyflakes" (list local-file)))) 
   (add-to-list 'flymake-allowed-file-name-masks 
-	       '("\\.py\\'" flymake-pyflakes-init))) 
+               '("\\.py\\'" flymake-pyflakes-init))) 
 
 (add-to-list 'load-path "~/.emacs.d/flymake-cursor")
 (load-library "flymake-cursor")
