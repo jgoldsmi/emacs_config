2e079928a839f617463b686b4aa2c88294a86279
Jonathon Goldsmith <jgoldsmith@wgen.net>
2011-11-21 13:10:46 -0500

    Add undo tree as a submodule

--- init.el
+++ init.el
@@ -2,6 +2,11 @@
 (menu-bar-mode -1)
 (setq indent-tabs-mode nil)
 
+;; undo-tree
+(add-to-list 'load-path "~/.emacs.d/undo-tree")
+(require 'undo-tree)
+(global-undo-tree-mode)
+
 ;; Evil
 (add-to-list 'load-path "~/.emacs.d/evil")
 (require 'evil)
