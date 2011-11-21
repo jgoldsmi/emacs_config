66d3d82d806a139c8a08534492a10441a427e1ed
Jonathon Goldsmith <jgoldsmith@wgen.net>
2011-11-21 14:24:09 -0500

    Add auto-complete as a submodule

--- init.el
+++ init.el
@@ -2,11 +2,13 @@
 (menu-bar-mode -1)
 (setq indent-tabs-mode nil)
 (setq inhibit-splash-screen t)
+(setq kill-whole-line t)
 
 ;; Pymacs/Rope
 ;; Install python-rope and python-ropemacs and pymacs via package manager
 (require 'pymacs)
 (pymacs-load "ropemacs" "rope-")
+(setq ropemacs-enable-autoimport t)
 
 ;; Magit
 (add-to-list 'load-path "~/.emacs.d/magit")
@@ -16,6 +18,12 @@
 (add-to-list 'load-path "~/.emacs.d/undo-tree")
 (require 'undo-tree)
 (global-undo-tree-mode)
+
+;; auto-complete
+(add-to-list 'load-path "~/.emacs.d/auto-complete")
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/auto-complete/dict")
+(ac-config-default)
 
 ;; Evil
 (add-to-list 'load-path "~/.emacs.d/evil")
