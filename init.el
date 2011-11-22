ea97d40ac418360569460e8f4438a985dc6273fb
Jonathon Goldsmith <jgoldsmith@wgen.net>
2011-11-22 09:51:35 -0500

    Add package.el

--- init.el
+++ init.el
@@ -54,3 +54,14 @@
 (add-to-list 'load-path "~/.emacs.d/evil")
 (require 'evil)
 (evil-mode 1)
+
+
+;;; This was installed by package-install.el.
+;;; This provides support for the package system and
+;;; interfacing with ELPA, the package archive.
+;;; Move this code earlier if you want to reference
+;;; packages in your .emacs.
+(when
+    (load
+     (expand-file-name "~/.emacs.d/elpa/package.el"))
+  (package-initialize))
