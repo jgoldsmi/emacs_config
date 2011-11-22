1cf59cb222c8ec3fa220e29e43c267031f04a9e0
Jonathon Goldsmith <jgoldsmith@wgen.net>
2011-11-22 11:09:42 -0500

    Add backup settings

--- init.el
+++ init.el
@@ -14,6 +14,14 @@
 (setq-default save-place t)
 (require 'saveplace)
 
+;; Backups
+(setq backup-directory-alist `(("." . "~/.emacs.d/saves")))
+(setq backup-by-copying t)
+(setq delete-old-versions t
+  kept-new-versions 6
+  kept-old-versions 2
+  version-control t)
+
 ;; Python Section
 
 ;; Pymacs/Rope
