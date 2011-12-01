2004aac031345b8fc7dcaee6fd21061ce3118b3a
Jonathon Goldsmith <jonathon.goldsmith@gmail.com>
2011-12-01 11:47:39 -0500

    Fix trailing whitespace in init.el

--- init.el
+++ init.el
@@ -52,16 +52,16 @@
 (setq ropemacs-enable-autoimport t)
 
 ;; Pyflakes checking
-(when (load "flymake" t) 
-  (defun flymake-pyflakes-init () 
-    (let* ((temp-file (flymake-init-create-temp-buffer-copy 
-                       'flymake-create-temp-inplace)) 
-           (local-file (file-relative-name 
-                        temp-file 
-                        (file-name-directory buffer-file-name)))) 
-      (list "pyflakes" (list local-file)))) 
-  (add-to-list 'flymake-allowed-file-name-masks 
-               '("\\.py\\'" flymake-pyflakes-init))) 
+(when (load "flymake" t)
+  (defun flymake-pyflakes-init ()
+    (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                       'flymake-create-temp-inplace))
+           (local-file (file-relative-name
+                        temp-file
+                        (file-name-directory buffer-file-name))))
+      (list "pyflakes" (list local-file))))
+  (add-to-list 'flymake-allowed-file-name-masks
+               '("\\.py\\'" flymake-pyflakes-init)))
 
 (add-to-list 'load-path "~/.emacs.d/flymake-cursor")
 (load-library "flymake-cursor")
