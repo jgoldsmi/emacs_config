ff87c550e46163a4521e68abb95cd740d271f479
Jonathon Goldsmith <jonathon.goldsmith@gmail.com>
2013-01-19 08:39:29 -0500

    Add el-get to init.el

--- init.el
+++ init.el
@@ -75,5 +75,22 @@
 ;; (define-key evil-normal-state-map "ZZ" (kbd "C-c C-c"))
 ;; (define-key minibuffer-local-map (kbd "C-w") 'backward-kill-word)
 
+;; el-get setup
+(setq el-get-user-package-directory "~/.emacs.d/el-get-init-files")
+;; (setq el-get-install-branch "4.stable")
+(setq el-get-install-branch "master")
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+(el-get 'sync)
+
+
+
 ;; emacs server
 (server-start)
