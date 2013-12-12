15d77be1c2fc4df7801c08b2fdfcf60cd4d01773
Jonathon Goldsmith <jonathon.goldsmith@blip.tv>
2013-12-12 10:44:41 -0500

    Get theme working again

--- el-get-init-files/init-solarized-theme.el
+++ el-get-init-files/init-solarized-theme.el
@@ -1,14 +1,6 @@
 (setq calendar-location-name "New York, NY")
 (setq calendar-latitude 40.7591)
 (setq calendar-longitude -73.9844)
-(defun load-solarized-light ()
-  "Load the solarized light theme."
-  (interactive)
-  (load-theme 'solarized-light t))
-(defun load-solarized-dark ()
-  "Load the solarized dark theme."
-  (interactive)
-  (load-theme 'solarized-dark t))
 (require 'theme-changer)
-(change-theme 'load-solarized-light 'load-solarized-dark)
+(change-theme 'solarized-light 'solarized-dark)
 
