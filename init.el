a34667f73a3a8531f0781b6c02a81ad80f5076f9
Jonathon Goldsmith <jonathon.goldsmith@blip.tv>
2013-12-13 11:30:50 -0500

    Add microemacs beginning/end-of-buffer bindings

--- init.el
+++ init.el
@@ -25,7 +25,8 @@
 (global-set-key "\C-c\C-k" 'kill-region)
 (global-set-key "\C-x\C-m" 'execute-extended-command)
 (global-set-key "\C-c\C-m" 'execute-extended-command)
-
+(global-set-key "\M-<" 'beginning-of-buffer)
+(global-set-key "\M->" 'end-of-buffer)
 
 ;; Remember place in files
 (setq save-place-file "~/.emacs.d/saveplace")
