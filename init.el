66d985e580cd09b8109c2b58faf4b855f946bf67
Jonathon Goldsmith <jonathon.goldsmith@gmail.com>
2013-12-12 21:47:07 -0500

    More Keybindings

--- init.el
+++ init.el
@@ -19,9 +19,12 @@
 ;; Keybindings
 (define-key global-map (kbd "RET") 'newline-and-indent)
 (global-set-key (kbd "C-z") 'scroll-down-command)
-(global-set-key "\C-w" 'backward-kill-word)
+(global-set-key "\C-w" 'backward-kill-word)  ;; From effective emacs
 (global-set-key "\C-x\C-k" 'kill-region)
 (global-set-key "\C-c\C-k" 'kill-region)
+(global-set-key "\C-x\C-m" 'execute-extended-command)
+(global-set-key "\C-c\C-m" 'execute-extended-command)
+
 
 ;; Remember place in files
 (setq save-place-file "~/.emacs.d/saveplace")
