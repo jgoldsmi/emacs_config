aa1728112d8d0df2d0b6bfbce667fe97d5fdbbc0
Jonathon Goldsmith <jonathon.goldsmith@blip.tv>
2013-12-17 09:59:31 -0500

    Diminish auto-complete
    This is on everywhere, so no need to show it

--- el-get-init-files/init-diminish.el
+++ el-get-init-files/init-diminish.el
@@ -2,3 +2,4 @@
 (eval-after-load "helm" '(diminish 'helm-mode))
 (eval-after-load "git-gutter" '(diminish 'git-gutter-mode))
 (eval-after-load "anzu" '(diminish 'anzu-mode))
+(eval-after-load "auto-complete" '(diminish 'auto-complete-mode))
