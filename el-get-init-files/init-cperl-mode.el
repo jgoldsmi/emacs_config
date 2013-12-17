6de551b0675a1ee30d094872498f6cb94e4d457f
Jonathon Goldsmith <jonathon.goldsmith@blip.tv>
2013-12-17 14:14:06 -0500

    Use a hook for cperl-mode so vars are defined

--- el-get-init-files/init-cperl-mode.el
+++ el-get-init-files/init-cperl-mode.el
@@ -1,4 +1,6 @@
 ;; Use cperl-mode instead of perl-mode
 (defalias 'perl-mode 'cperl-mode)
-(set-face-background 'cperl-array-face nil)
-(set-face-background 'cperl-hash-face nil)
+(add-hook 'cperl-mode-hook
+          (lambda ()
+            (set-face-background 'cperl-array-face nil)
+            (set-face-background 'cperl-hash-face nil)))
