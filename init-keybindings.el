316c943ff5f9244475fc92a11ee484a424b4d2b8
Jonathon Goldsmith <jonathon.goldsmith@gmail.com>
2013-12-22 11:15:13 -0500

    Clean up init-keybindings.el a bit

--- init-keybindings.el
+++ init-keybindings.el
@@ -2,13 +2,11 @@
 ;;; Commentary:
 
 ;;; Code:
-(define-key global-map (kbd "RET") 'newline-and-indent)
+(global-set-key (kbd "RET") 'newline-and-indent)
 (global-set-key (kbd "C-z") 'scroll-down-command)
 (global-set-key "\C-w" 'backward-kill-word)  ;; From effective emacs
 (global-set-key "\C-x\C-k" 'kill-region)
 (global-set-key "\C-c\C-k" 'kill-region)
-(global-set-key "\M-<" 'beginning-of-buffer)
-(global-set-key "\M->" 'end-of-buffer)
 (global-set-key "\C-x\\" 'align-regexp)
 
 (provide 'init-keybindings)
