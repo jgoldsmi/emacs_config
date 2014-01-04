4dcc12bcb85df05bf9bbf5fe9916f7d12cd68dec
Jonathon Goldsmith <jonathon.goldsmith@gmail.com>
2014-01-04 09:07:41 -0500

    Use helm when browsing the kill ring

--- el-get-init-files/init-helm.el
+++ el-get-init-files/init-helm.el
@@ -12,4 +12,5 @@
 (define-key global-map [remap find-file] 'helm-find-files)
 (define-key global-map [remap occur] 'helm-occur)
 (define-key global-map [remap list-buffers] 'helm-buffers-list)
+(define-key global-map [remap yank-pop] 'helm-show-kill-ring)
 (define-key isearch-mode-map [remap isearch-occur] 'helm-occur-from-isearch)
