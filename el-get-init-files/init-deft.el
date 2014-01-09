48317fc454af90e3dccb70eb43eedbc6e2b16deb
Jonathon Goldsmith <jonathon.goldsmith@blip.tv>
2014-01-09 09:30:34 -0500

    Add some todo states for use with deft

--- el-get-init-files/init-deft.el
+++ el-get-init-files/init-deft.el
@@ -1,3 +1,5 @@
+(setq org-todo-keywords
+      '((sequence "TODO(t!)" "STARTED(s@/!)" "|" "DONE(d!)" "CANCELLED(c@)")))
 (setq deft-directory "~/Dropbox/deft")
 (setq org-agenda-files (list "~/Dropbox/deft"))
 (setq deft-extension "org")
