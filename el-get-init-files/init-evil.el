f7206539d176ef4b3accc020d7436f420d0ebcef
Jonathon Goldsmith <jonathon.goldsmith@offerpop.com>
2014-08-11 10:13:06 -0400

    Add more evil bindings

--- el-get-init-files/init-evil.el
+++ el-get-init-files/init-evil.el
@@ -1,3 +1,5 @@
 (evil-mode 1)
 (evil-leader/set-key "w" 'evil-window-next)
 (evil-leader/set-key "d" 'evil-delete-buffer)
+(evil-leader/set-key "o" 'delete-other-windows)
+(define-key evil-normal-state-map "|" 'evil-window-vsplit)
