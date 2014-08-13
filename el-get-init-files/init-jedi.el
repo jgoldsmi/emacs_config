856a6bbe3e6d8c83cbaf6c682911d3f124ff36a2
Jonathon Goldsmith <jonathon.goldsmith@offerpop.com>
2014-08-13 11:00:08 -0400

    Use full setup for jedi
    Needed for complete-on-dot to work

--- el-get-init-files/init-jedi.el
+++ el-get-init-files/init-jedi.el
@@ -1,4 +1,4 @@
 (setq jedi:setup-keys t)
 (setq jedi:complete-on-dot t)
-(add-hook 'python-mode-hook 'jedi:ac-setup)
+(add-hook 'python-mode-hook 'jedi:setup)
 (add-to-list 'auto-mode-alist '("\\.psp\\'" . python-mode))
