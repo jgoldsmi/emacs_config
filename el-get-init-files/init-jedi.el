45757c8b2a5c0a34eb63ad01a879e15ded6f4996
Jonathon Goldsmith <jonathon.goldsmith@offerpop.com>
2014-08-13 11:57:25 -0400

    Have jedi use tags bindings for jumping

--- el-get-init-files/init-jedi.el
+++ el-get-init-files/init-jedi.el
@@ -1,4 +1,5 @@
 (setq jedi:setup-keys t)
 (setq jedi:complete-on-dot t)
+(setq jedi:use-shortcuts t)
 (add-hook 'python-mode-hook 'jedi:setup)
 (add-to-list 'auto-mode-alist '("\\.psp\\'" . python-mode))
