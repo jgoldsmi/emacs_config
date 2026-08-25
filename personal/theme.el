35e57caba5f9ff95ef6b22ffbb0b05c48d65ec66
Jonathon Goldsmith <jonathon.goldsmith@gmail.com>
2026-08-25 09:53:16 -0400

    Update theming
    - Use lambda line
    - Switch to Roboto font a la nano-emacs
    - Get rid of lines-tail, which was masking long lines

--- personal/theme.el
+++ personal/theme.el
@@ -1,3 +1,4 @@
+;; -*- lexical-binding: t; -*-
 ;; TODO: switch to lambda themes
 ;; (use-package nano-theme
 ;;  :ensure t)
@@ -12,6 +13,22 @@
    )
   )
 
+(use-package lambda-line
+  :vc (:url "https://codeberg.org/Lambda-Emacs/lambda-line" :rev :newest)
+  :custom
+  (lambda-line-position 'top)        ; default 'bottom — status line as a header
+  (lambda-line-abbrev t)             ; default nil — shorten major-mode names
+  (lambda-line-prefix-padding nil)   ; default t
+  (lambda-line-hspace "  ")          ; default " " — cushion
+  (lambda-line-space-top +.50)       ; default +.35 — vertical padding
+  (lambda-line-symbol-position 0.1)  ; default .067 — nudge the status glyph
+  (lambda-line-use-nerd-icons t)
+  :config
+  (lambda-line-mode)
+  (when (eq lambda-line-position 'top)
+    (setq-default mode-line-format (list "%_"))
+    (setq mode-line-format (list "%_"))))
+
 (use-package circadian
   :ensure t
   :config
@@ -21,4 +38,7 @@
                            (:sunset  . lambda-dark)))
   (circadian-setup))
 
-(set-frame-font "Maple Mono NF 13" nil t)
+(set-frame-font "RobotoMono Nerd Font" nil t)
+
+;; Customize whitespace-mode
+(setq whitespace-style '(face tabs empty trailing))   ; drop lines-tail, lambda blanks it out
