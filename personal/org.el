40d5e3cb2fae0d9dd58eeff5b10b0b3c772de368
Jonathon Goldsmith <jonathon.goldsmith@gmail.com>
2026-09-14 20:24:36 -0400

    Add org-roam daily template and org-todo-keywords
    todo-keywords is taken from doom-emacs

--- personal/org.el
+++ personal/org.el
@@ -7,10 +7,11 @@
    org-roam-directory (file-truename "~/org-roam")
    org-roam-dailies-directory "daily/"
    org-roam-dailies-capture-templates
-   '(("d" "default" entry
-      "* %?"
-      :target (file+head "%<%Y-%m-%d>.org"
-                         "#+title: %<%Y-%m-%d>\n"))))
+   '((d "default" entry
+        "* %?"
+        :target (file+head "%<%Y-%m-%d>.org"
+                           "#+title: %<%Y-%m-%d>\n\n* Morning Setup\n\n* Notes\n\n* Conclusion\n\n* TODOs")))
+   )
   (org-roam-db-autosync-mode)
   )
 
@@ -22,7 +23,44 @@
 
 (add-hook 'org-mode-hook #'jg/org-fill-setup)
 
-(setq org-todo-keywords '((sequence "TODO" "PROG" "|" "DONE")))
+;; Stolen from doom emacs
+(setq org-todo-keywords
+      '((sequence
+         "TODO(t)"  ; A task that needs doing & is ready to do
+         "PROJ(p)"  ; A project, which usually contains other tasks
+         "LOOP(r)"  ; A recurring task
+         "STRT(s)"  ; A task that is in progress
+         "WAIT(w)"  ; Something external is holding up this task
+         "HOLD(h)"  ; This task is paused/on hold because of me
+         "IDEA(i)"  ; An unconfirmed and unapproved task or notion
+         "|"
+         "DONE(d)"  ; Task successfully completed
+         "KILL(k)") ; Task was cancelled, aborted, or is no longer applicable
+        (sequence
+         "[ ](T)"   ; A task that needs doing
+         "[-](S)"   ; Task is in progress
+         "[?](W)"   ; Task is being held up or paused
+         "|"
+         "[X](D)")  ; Task was completed
+        (sequence
+         "|"
+         "OKAY(o)"
+         "YES(y)"
+         "NO(n)"))
+      org-todo-keyword-faces
+      '(("[-]"  . +org-todo-active)
+        ("STRT" . +org-todo-active)
+        ("[?]"  . +org-todo-onhold)
+        ("WAIT" . +org-todo-onhold)
+        ("HOLD" . +org-todo-onhold)
+        ("PROJ" . +org-todo-project)
+        ("NO"   . +org-todo-cancel)
+        ("KILL" . +org-todo-cancel)))
+(setq org-roam-dailies-capture-templates
+      '((d "default" entry
+           "* %?"
+           :target (file+head "%<%Y-%m-%d>.org"
+                              "#+title: %<%Y-%m-%d>\n\n* Morning Setup\n\n* Notes\n\n* Conclusion\n\n* TODOs"))))
 
 
 (defun jg/paste-slack-rich-text ()
@@ -43,3 +81,5 @@
 (global-set-key (kbd "C-c x f") 'org-roam-node-find)
 (global-set-key (kbd "C-c x i") 'org-roam-node-insert)
 (global-set-key (kbd "C-c x t") 'org-roam-dailies-goto-today)
+(global-set-key (kbd "C-c x m") 'org-roam-dailies-goto-tomorrow)
+(global-set-key (kbd "C-c x y") 'org-roam-dailies-goto-yesterday)
